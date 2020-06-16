8db77780575188b8a63bf7297316f05f2011c65a
Sravan Kumar <sravan.kumar@fyle.in>
2020-06-16 09:19:17 +0530

    minor

--- apps/netsuite/urls.py
+++ apps/netsuite/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import VendorView, AccountView, LocationView, DepartmentView, SubsidiaryView, BillView
+from .views import VendorView, AccountView, LocationView, DepartmentView, SubsidiaryView, BillView, BillScheduleView
 
 urlpatterns = [
     path('vendors/', VendorView.as_view()),
@@ -9,4 +9,5 @@ urlpatterns = [
     path('locations/', LocationView.as_view()),
     path('subsidiaries/', SubsidiaryView.as_view()),
     path('bills/', BillView.as_view()),
+    path('bills/trigger/', BillScheduleView.as_view()),
 ]
