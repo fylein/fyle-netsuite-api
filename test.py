4a99676dc01c3696666a4c2f668d8e0f7ceb08a3
ashwin1111 <winash1998@gmail.com>
2020-10-29 13:22:07 +0530

    bug fix

--- test.py
+++
@@ -1,6 +0,0 @@
-
-default_expense_attributes = ['CATEGORY', 'EMPLOYEE']
-default_destination_attributes = ['DEPARTMENT', 'LOCATION', 'CLASS']
-
-if 'COST_CENTER' not in default_expense_attributes and 'ASHWINTEST1' not in default_destination_attributes:
-    print('asd')
