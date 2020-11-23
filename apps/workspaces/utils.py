c22cd728f1ecbbf484babe7344850585052ddbee
Sravan Kumar <sravan.kumar@fyle.in>
2020-11-23 12:39:42 +0530

    rename

--- apps/workspaces/utils.py
+++ apps/workspaces/utils.py
@@ -29,6 +29,6 @@ def create_or_update_general_settings(general_settings_payload: Dict, workspace_
         }
     )
 
-    schedule_vendor_payment_creation(sync_payments=general_settings.import_projects, workspace_id=workspace_id)
+    schedule_vendor_payment_creation(sync_payments=general_settings.sync_payments, workspace_id=workspace_id)
 
     return general_settings
