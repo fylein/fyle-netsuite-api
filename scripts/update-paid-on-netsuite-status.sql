405ed22cfa6f64617b90074b38bec37e859a18b0
Sravan Kumar <sravan.kumar@fyle.in>
2020-12-18 13:50:20 +0530

    update script

--- scripts/update-paid-on-netsuite-status.sql
+++ scripts/update-paid-on-netsuite-status.sql
@@ -12,6 +12,11 @@ update expense_reports
 set paid_on_netsuite = True
 where expense_reports.paid_on_netsuite = False;
 
+-- journal_entries
+update journal_entries
+set paid_on_netsuite = True
+where journal_entries.paid_on_netsuite = False;
+
 -- expenses
 update expenses
 set paid_on_netsuite = True
