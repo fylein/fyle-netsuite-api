29b893755e6716b9055f148446f8d1f6840f8399
Shwetabh Kumar <shwetabh.kumar@fyle.in>
2021-02-10 18:27:33 +0530

    BugFix: Adding Or query to check account and Expense category

--- scripts/007-expense-category-cleanup.sql
+++ scripts/007-expense-category-cleanup.sql
@@ -1,5 +1,24 @@
 rollback;
 begin;
 
+insert into mapping_settings(source_field, destination_field, workspace_id, created_at, updated_at)
+select
+    'CATEGORY' as source_field,
+    'EXPENSE_CATEGORY' as destination_field,
+    w.id as workspace_id,
+    now(),
+    now()
+from workspaces w;
+
+insert into mapping_settings(source_field, destination_field, workspace_id, created_at, updated_at)
+select
+    'CATEGORY' as source_field,
+    'CCC_EXPENSE_CATEGORY' as destination_field,
+    w.id as workspace_id,
+    now(),
+    now()
+from workspaces w;
+
 update destination_attributes set attribute_type = 'EXPENSE_CATEGORY' where display_name = 'Expense Category';
+
 update destination_attributes set attribute_type = 'CCC_EXPENSE_CATEGORY' where display_name = 'Credit Card Expense Category';
