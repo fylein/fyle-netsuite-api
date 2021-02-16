ef81b93aa984aba2b8592e40ca7faa0897775559
Ashwin Thanaraj <37061471+ashwin1111@users.noreply.github.com>
2021-02-16 19:43:50 +0530

    Trigger auto map employees on click (#53)

--- apps/mappings/urls.py
+++ apps/mappings/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import GeneralMappingView, SubsidiaryMappingView
+from .views import GeneralMappingView, SubsidiaryMappingView, AutoMapEmployeeView
 
 urlpatterns = [
     path('subsidiaries/', SubsidiaryMappingView.as_view()),
-    path('general/', GeneralMappingView.as_view())
+    path('general/', GeneralMappingView.as_view()),
+    path('auto_map_employees/trigger/', AutoMapEmployeeView.as_view())
 ]
