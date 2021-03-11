10cae55484dd48cc9d237759e7224f55e86b66b6
Sravan Kumar <30795376+Sravanksk@users.noreply.github.com>
2021-03-11 13:20:29 +0530

    BugFix: Map Employee/Vendor before creating (#70)
    * BugFix: Map Employee/Vendor before creating

--- apps/mappings/tasks.py
+++ apps/mappings/tasks.py
@@ -1,6 +1,6 @@
 import logging
 import traceback
-from datetime import datetime, timedelta
+from datetime import datetime
 
 from typing import List, Dict
 
@@ -456,7 +456,6 @@ def async_auto_map_employees(workspace_id: int):
 
 def schedule_auto_map_employees(employee_mapping_preference: str, workspace_id: int):
     if employee_mapping_preference:
-        start_datetime = datetime.now()
 
         schedule, _ = Schedule.objects.update_or_create(
             func='apps.mappings.tasks.async_auto_map_employees',
@@ -464,7 +463,7 @@ def schedule_auto_map_employees(employee_mapping_preference: str, workspace_id: 
             defaults={
                 'schedule_type': Schedule.MINUTES,
                 'minutes': 24 * 60,
-                'next_run': start_datetime + timedelta(minutes=5)
+                'next_run': datetime.now()
             }
         )
     else:
@@ -501,7 +500,6 @@ def schedule_auto_map_ccc_employees(workspace_id: int):
     general_settings = WorkspaceGeneralSettings.objects.get(workspace_id=workspace_id)
 
     if general_settings.auto_map_employees:
-        start_datetime = datetime.now()
 
         schedule, _ = Schedule.objects.update_or_create(
             func='apps.mappings.tasks.async_auto_map_ccc_account',
@@ -509,7 +507,7 @@ def schedule_auto_map_ccc_employees(workspace_id: int):
             defaults={
                 'schedule_type': Schedule.MINUTES,
                 'minutes': 24 * 60,
-                'next_run': start_datetime + timedelta(minutes=5)
+                'next_run': datetime.now()
             }
         )
     else:
