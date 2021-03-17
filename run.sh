f1d178f3cc37c9adda8d3a78f777b311ed75b294
ashwinthanaraj <ashwin.t@fyle.in>
2021-03-17 13:48:02 +0530

    Change order

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Run db migrations
-python manage.py migrate
-
 # Creating the cache table
 python manage.py createcachetable --database cache_db
+
+# Run db migrations
+python manage.py migrate
 
 # Running development server
 python manage.py runserver 0.0.0.0:8000
