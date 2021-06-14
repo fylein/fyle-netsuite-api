b73b730cb3ac1e86209b6aa99d511b91a2efe813
ashwinthanaraj <ashwin.t@fyle.in>
2021-06-14 15:55:02 +0530

    Query with updated_at, url cleanup, optimise updates

--- apps/fyle/urls.py
+++ apps/fyle/urls.py
@@ -1,18 +1,28 @@
+import itertools
+
 from django.urls import path
 
 from .views import ExpenseGroupView, ExpenseGroupByIdView, ExpenseGroupScheduleView, ExpenseFieldsView, ExpenseView,\
     ExpenseCustomFieldsView, ExpenseGroupSettingsView, SyncFyleDimensionView, RefreshFyleDimensionView,\
     ExpenseGroupCountView
 
-urlpatterns = [
+expense_groups_paths = [
     path('expense_groups/', ExpenseGroupView.as_view()),
     path('expense_groups/count/', ExpenseGroupCountView.as_view()),
     path('expense_groups/trigger/', ExpenseGroupScheduleView.as_view()),
     path('expense_groups/<int:pk>/', ExpenseGroupByIdView.as_view()),
     path('expense_groups/<int:expense_group_id>/expenses/', ExpenseView.as_view()),
-    path('expense_custom_fields/', ExpenseCustomFieldsView.as_view()),
-    path('expense_fields/', ExpenseFieldsView.as_view()),
-    path('expense_group_settings/', ExpenseGroupSettingsView.as_view()),
+    path('expense_group_settings/', ExpenseGroupSettingsView.as_view())
+]
+
+fyle_dimension_paths = [
     path('sync_dimensions/', SyncFyleDimensionView.as_view()),
     path('refresh_dimensions/', RefreshFyleDimensionView.as_view())
 ]
+
+other_paths = [
+    path('expense_custom_fields/', ExpenseCustomFieldsView.as_view()),
+    path('expense_fields/', ExpenseFieldsView.as_view())
+]
+
+urlpatterns = list(itertools.chain(expense_groups_paths, fyle_dimension_paths, other_paths))
