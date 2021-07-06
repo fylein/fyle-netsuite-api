9fa18554e0e72cd5a910efcd1b77e3a492857811
Sravan Kumar <sravan.kumar@fyle.in>
2021-07-06 14:21:12 +0530

    update custom fields

--- apps/fyle/connector.py
+++ apps/fyle/connector.py
@@ -306,7 +306,8 @@ class FyleConnector:
                 count = count + 1
 
             ExpenseAttribute.bulk_create_or_update_expense_attributes(
-                expense_custom_field_attributes, custom_field['name'].upper().replace(' ', '_'), self.workspace_id
+                expense_custom_field_attributes, custom_field['name'].upper().replace(' ', '_'), self.workspace_id,
+                update=True
             )
 
         return []
