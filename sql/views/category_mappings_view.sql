19f6bd54345d5ebbe9ccb48b35219da442e9bd72
Shwetabh Kumar <shwetabh002@gmail.com>
2021-08-02 18:50:30 +0530

    Removing references of CCC_EXPENSE_CATEGORY and CCC_ACCOUNT from Mappings

--- sql/views/category_mappings_view.sql
+++ sql/views/category_mappings_view.sql
@@ -1,7 +1,8 @@
 -- View to help migrate category mappings from mappings table to category_mappings table
+drop view if exists category_mappings_view_temp;
 drop view if exists category_mappings_view;
 
-create or replace view category_mappings_view as
+create or replace view category_mappings_view_temp as
 select
     m.source_id as source_category_id,
     max(
@@ -18,3 +19,24 @@ from
 where m.source_type = 'CATEGORY'
 group by
     m.source_id, m.workspace_id;
+
+create or replace view category_mappings_view as
+select
+    cm.source_category_id,
+    ndeh.id as destination_expense_head_id,
+    nda.id as destination_account_id,
+    cm.workspace_id,
+    cm.created_at as created_at,
+    cm.updated_at as updated_at
+from
+    category_mappings_view_temp cm
+left join destination_attributes da on cm.destination_account_id = da.id
+left join destination_attributes nda on (
+    da.destination_id =  nda.destination_id and nda.attribute_type = 'ACCOUNT' and nda.workspace_id = da.workspace_id
+)
+left join destination_attributes deh on cm.destination_expense_head_id = deh.id
+left join destination_attributes ndeh on (
+    deh.destination_id = ndeh.destination_id and ndeh.attribute_type = 'EXPENSE_CATEGORY' and ndeh.workspace_id = deh.workspace_id
+)
+where da.attribute_type in ('CCC_ACCOUNT', 'ACCOUNT')
+or deh.attribute_type in ('CCC_EXPENSE_CATEGORY', 'EXPENSE_CATEGORY');
