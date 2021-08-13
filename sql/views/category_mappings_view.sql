db221bee01ccb592622a6ba734fcb7086cf046f7
Shwetabh Kumar <shwetabh002@gmail.com>
2021-08-13 10:42:00 +0530

    Category Mapping View

--- sql/views/category_mappings_view.sql
+++ sql/views/category_mappings_view.sql
@@ -40,3 +40,58 @@ left join destination_attributes ndeh on (
 )
 where da.attribute_type in ('CCC_ACCOUNT', 'ACCOUNT')
 or deh.attribute_type in ('CCC_EXPENSE_CATEGORY', 'EXPENSE_CATEGORY');
+
+
+-- Checking for any mismatching expense head names in old and new view
+select count(*) from category_mappings_view cm
+left join category_mappings_view_temp cmt on cm.source_category_id = cmt.source_category_id
+left join destination_attributes newda on newda.id = cm.destination_expense_head_id
+left join destination_attributes oldda on oldda.id = cmt.destination_expense_head_id
+where newda.value <> oldda.value;
+
+
+-- Checking for any mismatching account names in old and new view
+select count(*) from category_mappings_view cm
+left join category_mappings_view_temp cmt on cm.source_category_id = cmt.source_category_id
+left join destination_attributes newda on newda.id = cm.destination_account_id
+left join destination_attributes oldda on oldda.id = cmt.destination_account_id
+where newda.value <> oldda.value;
+
+
+--- Columns which have both expense_head_id and account_id are equal
+select count(*) from category_mappings_view_temp
+where destination_expense_head_id is not null and destination_account_id is not null;
+
+select count(*) from category_mappings_view
+where destination_expense_head_id is not null and destination_account_id is not null;
+
+
+--- Same number of destination_account
+select count(*) from category_mappings_view_temp where destination_account_id is not null;
+select count(*) from category_mappings_view where destination_account_id is not null;
+
+
+--- Same number of destination_expense_head
+select count(*) from category_mappings_view_temp where destination_expense_head_id is not null;
+select count(*) from category_mappings_view where destination_expense_head_id is not null;
+
+
+--- No CCC_ACCOUNT in the new view
+select count(*) from (
+    select cm.destination_expense_head_id, cm.destination_account_id, da.value, da.attribute_type
+    from category_mappings_view cm left join destination_attributes da on cm.destination_account_id = da.id
+    where da.attribute_type = 'CCC_ACCOUNT'
+) s;
+
+
+--- No CCC_EXPENSE_CATEGORY in the new view
+select count(*) from (
+    select cm.destination_expense_head_id, cm.destination_account_id, da.value, da.attribute_type
+    from category_mappings_view cm left join destination_attributes da on cm.destination_expense_head_id_id = da.id
+    where da.attribute_type = 'CCC_EXPENSE_CATEGORY'
+) s;
+
+
+--- No of rows match
+select count(*) from category_mappings_view_temp;
+select count(*) from category_mappings_view;
