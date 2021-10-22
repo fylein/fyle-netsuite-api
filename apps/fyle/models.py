e9cd90660ceeb7fc736a6ca313022e9f96363b97
Shwetabh Kumar <shwetabh.kumar@fyle.in>
2021-10-22 13:27:05 +0530

    adding workspace_id to logger

--- apps/fyle/models.py
+++ apps/fyle/models.py
@@ -151,7 +151,7 @@ class Expense(models.Model):
                 eliminated_expenses.append(expense['id'])
 
         if eliminated_expenses:
-            logger.error('Expenses with ids {} are not eligible for import'.format(eliminated_expenses))
+            logger.error('Expenses with ids {} are not eligible for import, workspace_id {}'.format(eliminated_expenses, workspace_id))
 
         return expense_objects
 
