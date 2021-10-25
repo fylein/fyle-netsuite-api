26957174fb0b6dea7b37931bb26ef4e94ae3e463
Nilesh Pant <nileshpant6969@gmail.com>
2021-10-25 10:50:39 +0530

    comments resolvedl

--- apps/netsuite/tasks.py
+++ apps/netsuite/tasks.py
@@ -776,13 +776,14 @@ def __validate_expense_group(expense_group: ExpenseGroup, configuration: Configu
     category_mapping_errors = __validate_category_mapping(expense_group, configuration)
 
     # Tax Group Mapping
-    tax_group_mapping_error = __validate_tax_group_mapping(expense_group, configuration)
+    if configuration.import_tax_items:
+        tax_group_mapping_errors = __validate_tax_group_mapping(expense_group, configuration)
 
-    bulk_errors = list(
-        itertools.chain(
-            general_mapping_errors, subsidiary_mapping_errors, employee_mapping_errors, category_mapping_errors, tax_group_mapping_error
+        bulk_errors = list(
+            itertools.chain(
+                general_mapping_errors, subsidiary_mapping_errors, employee_mapping_errors, category_mapping_errors, tax_group_mapping_error
+            )
         )
-    )
 
     if bulk_errors:
         raise BulkError('Mappings are missing', bulk_errors)
