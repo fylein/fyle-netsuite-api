a7173302fd7d70827b4bf0a90a117a0f6e1d91f9
Nilesh Pant <nileshpant6969@gmail.com>
2021-11-16 11:28:43 +0530

    minor fixes

--- reset_db.sh
+++ reset_db.sh
@@ -1,5 +1,5 @@
-echo "Replacing Data with Dummy Data"
+echo "-------------Populating Test Datebase---------------"
 PGPASSWORD=postgres psql postgres -c "drop database test_netsuite";
 PGPASSWORD=postgres psql postgres -c "create database test_netsuite";
 PGPASSWORD=postgres psql test_netsuite -c "\i 'netsuite.sql'";
-echo "Started Testing"
+echo "---------------------Testing-------------------------"
