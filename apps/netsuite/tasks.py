f783148f19a43cef69feab67f44e1bb90120b1f1
Shwetabh Kumar <shwetabh.kumar@fyle.in>
2021-11-18 09:36:25 +0530

    Removing Print Statements

--- apps/netsuite/tasks.py
+++ apps/netsuite/tasks.py
@@ -219,8 +219,6 @@ def create_bill(expense_group, task_log_id):
     configuration: Configuration = Configuration.objects.get(workspace_id=expense_group.workspace_id)
     general_mappings: GeneralMapping = GeneralMapping.objects.filter(workspace_id=expense_group.workspace_id).first()
 
-    print(expense_group.expenses.all())
-    print(expense_group.id)
     try:
         netsuite_credentials = NetSuiteCredentials.objects.get(workspace_id=expense_group.workspace_id)
 
