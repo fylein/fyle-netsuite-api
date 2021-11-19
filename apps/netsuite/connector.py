ab400bb4e4bbc55ed00a79b3e223be1929c24d63
ashwinthanaraj <ashwin.t@fyle.in>
2021-11-19 11:46:49 +0530

    Add Deferred Expense as Accounts

--- apps/netsuite/connector.py
+++ apps/netsuite/connector.py
@@ -113,7 +113,7 @@ class NetSuiteConnector:
 
                 if account['acctType'] == '_expense' or account['acctType'] == '_costOfGoodsSold' or \
                         account['acctType'] == '_otherCurrentAsset' or account['acctType'] == '_otherExpense'or \
-                            account['acctType'] == '_fixedAsset':
+                            account['acctType'] == '_fixedAsset' or account['acctType'] == '_deferredExpense':
                     attributes['account'].append({
                         'attribute_type': 'ACCOUNT',
                         'display_name': 'Account',
