462c5895524d60cd9453e7f16785544501e39906
Shwetabh Kumar <shwetabh.kumar@fyle.in>
2021-12-13 18:03:22 +0530

    Infra(Addition): Adding script to auto generate migration fixtures (#193)
    * Adding scripts to auto generate migration fixtures
    
    * Infra(Addition): Adding script to auto generate migration fixtures
    
    * Feature(Improvement): Adding workflow to run extra scripts while creating migration fixture
    
    * Doc(Addition): Adding steps to generate migration file in README

--- tests/sql_fixtures/reset_db_fixtures/reset_db.sh
+++ tests/sql_fixtures/reset_db_fixtures/reset_db.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 echo "-------------Populating Test Datebase---------------"
 PGPASSWORD=postgres psql -h $DB_HOST -U postgres -c "drop database test_netsuite_db";
 PGPASSWORD=postgres psql -h $DB_HOST -U postgres -c "create database test_netsuite_db";
