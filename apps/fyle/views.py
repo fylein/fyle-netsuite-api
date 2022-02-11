22a487b7aa07aaea6d2b57b77a68a91e1996862f
Nilesh Pant <nileshpant6969@gmail.com>
2022-02-11 12:57:20 +0530

    Remove Corporate Card from Fyle View

--- apps/fyle/views.py
+++ apps/fyle/views.py
@@ -105,7 +105,7 @@ class FyleFieldsView(generics.ListAPIView):
     serializer_class = ExpenseFieldSerializer
 
     def get(self, request, *args, **kwargs):
-        default_attributes = ['EMPLOYEE', 'CATEGORY', 'PROJECT', 'COST_CENTER', 'TAX_GROUP']
+        default_attributes = ['EMPLOYEE', 'CATEGORY', 'PROJECT', 'COST_CENTER', 'TAX_GROUP', 'CORPORATE_CARD']
 
         attributes = ExpenseAttribute.objects.filter(
             ~Q(attribute_type__in=default_attributes),
