c987d0277177da43bdd2982e905531ced038194b
Sravan Kumar <30795376+Sravanksk@users.noreply.github.com>
2022-03-24 15:51:13 +0530

    catch representingsubsidiary exception during vendor creation (#248)
    catch representingsubsidiary exception during vendor creation (#248)

--- apps/netsuite/connector.py
+++ apps/netsuite/connector.py
@@ -438,7 +438,18 @@ class NetSuiteConnector:
             'externalId': vendor.detail['user_id'] if vendor else merchant
         }
 
-        return self.connection.vendors.post(vendor)
+        vendor_response = None
+        try:
+            vendor_response = self.connection.vendors.post(vendor)
+        except NetSuiteRequestError as exception:
+            logger.exception({'error': exception})
+            detail = json.dumps(exception.__dict__)
+            detail = json.loads(detail)
+            if 'representingsubsidiary' in detail['message']:
+                vendor['representingSubsidiary']['internalId'] = None
+                vendor_response = self.connection.vendors.post(vendor)
+
+        return vendor_response
 
     def sync_employees(self):
         """
