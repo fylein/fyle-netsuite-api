c90dc7d4e0af8c192ae9053bf44b83f734d5dd7f
labhvam5 <labhvam.s@fyle.in>
2022-05-05 17:17:52 +0530

    location department and class id changes for JE

--- apps/netsuite/models.py
+++ apps/netsuite/models.py
@@ -1055,8 +1055,8 @@ class JournalEntryLineItem(models.Model):
             else:
                 class_id = get_class_id_or_none(expense_group, lineitem)
 
-
-            department_id = get_department_id_or_none(expense_group, lineitem)
+            if expense_group.fund_source == 'CCC':
+                department_id = get_department_id_or_none(expense_group, lineitem)
 
             if general_mappings.use_employee_department and general_mappings.department_level in ('ALL', 'TRANSACTION_LINE') \
                 and employee_field_mapping == 'EMPLOYEE':
@@ -1066,17 +1066,24 @@ class JournalEntryLineItem(models.Model):
                 ).first()
                 if employee_mapping and employee_mapping.destination_employee:
                     department_id = employee_mapping.destination_employee.detail.get('department_id')
-            
-            location_id = get_location_id_or_none(expense_group, lineitem)
 
-            if expense_group.fund_source == 'CCC' and general_mappings.use_employee_location and\
-                    general_mappings.location_level in ('ALL', 'TRANSACTION_LINE'):
+            elif expense_group.fund_source == 'PERSONAL':
+                department_id = get_department_id_or_none(expense_group, lineitem)
+
+            if expense_group.fund_source == 'CCC':
+                location_id = get_location_id_or_none(expense_group, lineitem)
+
+            if  general_mappings.use_employee_location and general_mappings.location_level in ('ALL', 'TRANSACTION_LINE')\
+                and employee_field_mapping == 'EMPLOYEE':
                 employee_mapping = EmployeeMapping.objects.filter(
                     source_employee__value=expense_group.description.get('employee_email'),
                     workspace_id=expense_group.workspace_id
                 ).first()
                 if employee_mapping and employee_mapping.destination_employee:
                     location_id = employee_mapping.destination_employee.detail.get('location_id')
+
+            elif expense_group.fund_source == 'PERSONAL':
+                location_id = get_location_id_or_none(expense_group, lineitem)
 
             if not location_id and general_mappings.location_id:
                 location_id = general_mappings.location_id
