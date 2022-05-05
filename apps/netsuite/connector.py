6e587fd2d55419cca87bf2f4ad808d6c543c8ece
Nilesh Pant <nileshpant6969@gmail.com>
2022-05-05 16:19:05 +0530

    minor fix

--- apps/netsuite/connector.py
+++ apps/netsuite/connector.py
@@ -1292,7 +1292,7 @@ class NetSuiteConnector:
             'amount': None,
             'memo': expense_report.memo,
             'complete': None,
-            #'supervisorApproval': True,
+            'supervisorApproval': True,
             'accountingApproval': True,
             'useMultiCurrency': None,
             'tax2Amt': None,
