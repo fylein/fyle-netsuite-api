28fdc9eb8b45e9d5c09f027fb01bc2062e7156cb
Shwetabh Kumar <shwetabh.kumar@fyle.in>
2022-07-26 19:01:10 +0530

    Fixed the failed tests (#292)

--- tests/test_netsuite/test_connector.py
+++ tests/test_netsuite/test_connector.py
@@ -108,7 +108,7 @@ def test_sync_employee(add_netsuite_credentials):
     netsuite_connection.sync_employees()
 
     new_employee_count = DestinationAttribute.objects.filter(workspace_id=49, attribute_type='EMPLOYEE').count()
-    assert new_employee_count == 14
+    assert new_employee_count == 13
 
 @pytest.mark.django_db()
 def test_sync_accounts(add_netsuite_credentials):
