a39a430b5b4b4080f455f7ed2192e9a10c578b45
Sravan Kumar <30795376+Sravanksk@users.noreply.github.com>
2022-09-19 13:05:03 +0530

    fi

--- apps/netsuite/connector.py
+++ apps/netsuite/connector.py
@@ -658,9 +658,7 @@ class NetSuiteConnector:
             for tax_group in tax_groups:
                 if not tax_group['isInactive'] and tax_group['itemId']:
                     tax_type = tax_group['taxType']['name'] if tax_group['taxType'] else None
-                    print('RAte -', tax_group['rate'])
-                    tax_rate = tax_group['rate'].replace('%', '') if tax_group['rate'] else 0
-                    tax_rate = float(tax_rate)
+                    tax_rate = float(tax_group['rate'] if tax_group['rate'] else 0)
 
                     value = self.get_tax_code_name(tax_group['itemId'], tax_type, tax_rate)
                     if tax_rate >= 0:
