a7ab51475196076b120cc1f91e96c0bb1da61021
Nilesh Pant <nileshpant6969@gmail.com>
2022-12-23 10:38:02 +0530

    Bug Fix: Post Merchant Issue removed updated at filter

--- apps/mappings/tasks.py
+++ apps/mappings/tasks.py
@@ -1006,12 +1006,9 @@ def post_merchants(platform_connection: PlatformConnector, workspace_id: int, fi
         netsuite_attributes = DestinationAttribute.objects.filter(
             attribute_type='VENDOR', workspace_id=workspace_id).order_by('value', 'id')
     else:
-        merchant = platform_connection.merchants.get()
-        merchant_updated_at = parser.isoparse(merchant['updated_at']).strftime('%Y-%m-%d %H:%M:%S.%f')
         netsuite_attributes = DestinationAttribute.objects.filter(
             attribute_type='VENDOR',
             workspace_id=workspace_id,
-            updated_at__gte=merchant_updated_at
         ).order_by('value', 'id')
 
     netsuite_attributes = remove_duplicates(netsuite_attributes)
