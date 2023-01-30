efb08f0b70a05cbda0001101b25c67e109ec0b59
ashwin1111 <winash1998@gmail.com>
2023-01-30 20:30:43 +0530

    fix lint

--- fyle_netsuite_api/settings.py
+++ fyle_netsuite_api/settings.py
@@ -178,7 +178,8 @@ Q_CLUSTER = {
     'name': 'fyle_netsuite_api',
     'save_limit': 0,
     'workers': int(os.environ.get('NO_WORKERS', 4)),
-    # How many tasks are kept in memory by a single cluster. Helps balance the workload and the memory overhead of each individual cluster
+    # How many tasks are kept in memory by a single cluster. 
+    # Helps balance the workload and the memory overhead of each individual cluster
     'queue_limit': 10,
     'cached': False,
     'orm': 'default',
@@ -189,7 +190,8 @@ Q_CLUSTER = {
     'retry': 14400,
     'timeout': 3600,
     'catch_up': False,
-    # The number of tasks a worker will process before recycling . Useful to release memory resources on a regular basis.
+    # The number of tasks a worker will process before recycling. 
+    # Useful to release memory resources on a regular basis.
     'recycle': 50,
     'max_rss': '100mb'
 }
