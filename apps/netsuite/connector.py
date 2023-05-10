3ca9bfa2c705c7cab8d7562ad77eb76defe8a075
ashwin1111 <winash1998@gmail.com>
2023-05-10 13:41:34 +0530

    additional safe checks

--- apps/netsuite/connector.py
+++ apps/netsuite/connector.py
@@ -69,7 +69,7 @@ class NetSuiteConnector:
 
         logger.info('Charge Card Error - %s', response)
         code = response['error']['code'] if 'error' in response and 'code' in response['error'] else response['code']
-        message = response['error']['message']['message']
+        message = response['error']['message']['message'] if 'error' in response and 'message' in response['error'] and 'message' in response['error']['message'] else response['message']['message']
 
         return code, message
     
