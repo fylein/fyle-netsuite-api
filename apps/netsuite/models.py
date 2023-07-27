9eb6bd20d1e7d561d0c883d3dc25703b975760a9
ruuushhh <rushikeshtayade2001@gmail.com>
2023-07-27 10:44:48 +0530

    Added check for department id

--- apps/netsuite/models.py
+++ apps/netsuite/models.py
@@ -581,7 +581,8 @@ class CreditCardCharge(models.Model):
 
         if general_mappings.use_employee_department and general_mappings.department_level in (
             'ALL', 'TRANSACTION_BODY') and employee_field_mapping == 'EMPLOYEE':
-            department_id = employee_mapping.destination_employee.detail.get('department_id')
+            if employee_mapping.destination_employee.detail.get('department_id'):
+                department_id = employee_mapping.destination_employee.detail.get('department_id')
 
         credit_charge_object, _ = CreditCardCharge.objects.update_or_create(
             expense_group=expense_group,
