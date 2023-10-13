aa7c23132692b7e514de91cb356645dd78e6d5fd
Ashutosh singh <55102089+Ashutosh619-sudo@users.noreply.github.com>
2023-10-13 15:29:28 +0530

    onboarding state and script  (#425)
    * onboarding state implementation
    
    * tests migrations
    
    * added onboarding state
    
    * changed comment
    
    * added subsidiary state to onboarding state
    
    * changed script to add subsidiary state and fixed some bug
    
    ---------
    
    Co-authored-by: Ashutosh619-sudo <ashutoshsinghkrkt@gmail.com>
    Co-authored-by: Nilesh Pant <nileshpant6969@gmail.com>

---
+++ scripts/sql/scripts/019-add-onboarding-state.sql
@@ -0,0 +1,99 @@
+-- Create a view for joined on all settings tables to figure out onboarding progress
+create or replace view all_settings_view as 
+select 
+    w.id as workspace_id,
+    wgs.id as configuration_id,
+    gm.id as general_mappings_id,
+    qc.id as netsuite_creds_id,
+    sm.id as subsidiary_id
+from workspaces w 
+left join 
+    configurations wgs on w.id = wgs.workspace_id 
+left join 
+    netsuite_credentials nc on qc.workspace_id = w.id 
+left join 
+    general_mappings gm on gm.workspace_id = w.id
+left join 
+    subsidiary_mappings sm on sm.workspace_id = w.id
+where w.onboarding_state = 'CONNECTION';
+
+begin; -- Start Transaction Block
+
+-- Count of all workspaces where netsuite are present, configuration is present and general mappings are present
+select 
+    'NC=TRUE, C=TRUE, GM=TRUE, SM=True' as setting, count(*) 
+from all_settings_view 
+where 
+    configuration_id is not null and general_mappings_id is not null and netsuite_creds_id is not null and subsidiary_id is not null;
+
+--- Update all of the above to have onboarding state set to 'COMPLETE'
+update workspaces 
+set 
+    onboarding_state = 'COMPLETE' 
+where id in (
+    select 
+        workspace_id 
+    from all_settings_view 
+    where 
+        configuration_id is not null and general_mappings_id is not null and netsuite_creds_id is not null and subsidiary_id is not null
+);
+
+-- Count of all workspaces where netsuite cred is present and general mapping and credentials and subsidiary are not present.
+select 
+    'NC=TRUE, C=FALSE, GM=FALSE, SM=FALSE' as setting, count(*)
+from all_settings_view
+where
+    configuration_id is null and general_mappings_id is null and netsuite_creds_id is not null and subsidiary_id is null;
+
+-- Update all of the above to have onboarding state set to 'SUBSIDIARY'
+update workspaces
+set
+    onboarding_state = 'SUBSIDIARY'
+where id in (
+    select
+        workspace_id
+    from all_settings_view
+    where 
+        configuration_id is null and general_mappings_id is null and netsuite_creds_id is not null and subsidiary_id is null
+);
+
+-- Count of all workspaces where netsuite cred and subsidiary are present, configuration is not present and general mappings are not present
+select 
+    'NC=TRUE, C=FALSE, GM=FALSE, SM=TRUE' as settings, count(*) 
+from all_settings_view 
+where 
+    configuration_id is null and general_mappings_id is null and netsuite_creds_id is not null and subsidiary_id is not null;
+
+--- Update all of the above to have onboarding state set to 'MAP_EMPLOYEES'
+update workspaces 
+set 
+    onboarding_state = 'MAP_EMPLOYEES' 
+where id in (
+    select 
+        workspace_id 
+    from all_settings_view 
+    where 
+        configuration_id is null and general_mappings_id is null and netsuite_creds_id is not null and subsidiary_id is not null
+);
+
+
+-- Count of all workspaces where netsuite are present, configuration is present, subsidiary is present and general mappings are not present
+select 
+    'NC=TRUE, C=TRUE, GM=FALSE, SM=TRUE' as settings, count(*) 
+from all_settings_view 
+where 
+    configuration_id is not null and general_mappings_id is null and netsuite_creds_id is not null and subsidiary_id is not null;
+
+--- Update all of the above to have onboarding state set to 'EXPORT_SETTINGS'
+update workspaces 
+set 
+    onboarding_state = 'EXPORT_SETTINGS' 
+where id in (
+    select 
+        workspace_id 
+    from all_settings_view 
+    where 
+        configuration_id is not null and general_mappings_id is null and netsuite_creds_id is not null and subsidiary_id is not null
+);
+
+
