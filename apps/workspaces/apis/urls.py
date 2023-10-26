63102f81c8cbf8eb51c6b8689b1f50d81b9aae38
Ashutosh singh <55102089+Ashutosh619-sudo@users.noreply.github.com>
2023-10-26 11:39:53 +0530

    V2 Export setting api (#433)
    * onboarding state implementation
    
    * tests migrations
    
    * added onboarding state
    
    * changed comment
    
    * added subsidiary state to onboarding state
    
    * changed script to add subsidiary state and fixed some bug
    
    * bug fix
    
    * state change on connection and subsidiary change
    
    * map employees v2 api
    
    * map_employees typos
    
    * bug fix
    
    * export setting changes
    
    * export settings V2 api
    
    * added test for export settings api
    
    * resolved comments
    
    ---------
    
    Co-authored-by: Ashutosh619-sudo <ashutoshsinghkrkt@gmail.com>
    Co-authored-by: Nilesh Pant <nileshpant6969@gmail.com>

--- apps/workspaces/apis/urls.py
+++ apps/workspaces/apis/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
 from apps.workspaces.apis.map_employees.views import MapEmployeesView
+from apps.workspaces.apis.export_settings.views import ExportSettingsView
 
 
 urlpatterns = [
-    path('<int:workspace_id>/map_employees/', MapEmployeesView.as_view()),
+    path('<int:workspace_id>/map_employees/', MapEmployeesView.as_view(), name='map-employees'),
+    path('<int:workspace_id>/export_settings/', ExportSettingsView.as_view(), name='export-settings'),
 ]
