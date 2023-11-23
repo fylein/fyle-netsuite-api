fce7ebb4d30428909d0d31351bc4902aa73aca7e
Ashutosh619-sudo <ashutoshsinghrkt@gmail.com>
2023-11-23 16:40:44 +0530

    minor changes

--- apps/fyle/tasks.py
+++ apps/fyle/tasks.py
@@ -35,7 +35,7 @@ def get_task_log_and_fund_source(workspace_id: int):
 
     configuration = Configuration.objects.get(workspace_id=workspace_id)
     fund_source = ['PERSONAL']
-    if configuration.corporate_credit_card_expenses_object:
+    if configuration.corporate_credit_card_expenses_object is not None:
         fund_source.append('CCC')
 
     return task_log, fund_source, configuration
