65b91e41d8446ced76d5ddfba76355b37e09c5b7
Ashutosh619-sudo <ashutoshsinghrkt@gmail.com>
2023-11-29 19:14:59 +0530

    new task polling view

--- apps/tasks/views.py
+++ apps/tasks/views.py
@@ -1,8 +1,10 @@
+from fyle_netsuite_api.utils import LookupFieldMixin
 from rest_framework import generics
 
 from .helpers import filter_tasks_by_params
 from .models import TaskLog
 from .serializers import TaskLogSerializer
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class TasksView(generics.ListAPIView):
@@ -25,3 +27,13 @@ class TasksByExpenseGroupIdView(generics.RetrieveAPIView):
     serializer_class = TaskLogSerializer
     lookup_field = 'expense_group_id'
     queryset = TaskLog.objects.all()
+
+
+class NewTaskView(LookupFieldMixin, generics.ListAPIView):
+
+    serializer_class = TaskLogSerializer
+    queryset = TaskLog.objects.all()
+    pagination_class = None
+    filter_backends = (DjangoFilterBackend,)
+    filterset_fields = {'task':{'in','exact'}, 'expense_group_ids':{'in', 'exact'}, 'status': {'in', 'exact'}}
+    ordering = ['-updated_at']
