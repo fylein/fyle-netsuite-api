c3ad2455fd23808c1647a2aa6230d9f441c6aaca
Ashutosh singh <55102089+Ashutosh619-sudo@users.noreply.github.com>
2023-11-29 23:59:24 -0800

    New task view bug fix (#466)
    * new task polling view
    
    * new task bug fix
    
    * minor change

--- apps/tasks/urls.py
+++ apps/tasks/urls.py
@@ -5,5 +5,5 @@ from .views import TasksView, TasksByExpenseGroupIdView, NewTaskView
 urlpatterns = [
     path('expense_group/<int:expense_group_id>/', TasksByExpenseGroupIdView.as_view(), name='task-by-expense-group-id'),
     path('all/', TasksView.as_view(), name='all-tasks'),
-    path('v2/all,', NewTaskView.as_view(), name='new-all-tasks')
+    path('v2/all/', NewTaskView.as_view(), name='new-all-tasks')
 ]
