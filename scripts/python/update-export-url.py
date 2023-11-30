15814ef7546cd8205671718fc4b2789b44bf1e0d
Ashutosh619-sudo <ashutoshsinghrkt@gmail.com>
2023-11-30 13:42:28 +0530

    script changes

--- scripts/python/update-export-url.py
+++ scripts/python/update-export-url.py
@@ -10,11 +10,12 @@ prod_workspaces = Workspace.objects.exclude(
 for workspace in prod_workspaces:
     page_size = 200
     expense_group_counts = ExpenseGroup.objects.filter(workspace_id=workspace.id, response_logs__isnull=False).count()
-    for offset in range(0, expense_group_counts, page_size):
-        expense_to_be_updated = []
-        limit = offset + page_size
-        paginated_expense_groups = ExpenseGroup.objects.filter(workspace_id=workspace.id, response_logs__isnull=False)[offset:limit]
-        for expense_group in paginated_expense_groups:
-            netsuite_credentials = NetSuiteCredentials.objects.get(workspace_id=workspace.id)
-            expense_group.export_url = generate_netsuite_export_url(response_logs=expense_group.response_logs, netsuite_credentials=netsuite_credentials)
-            expense_group.save()
+    netsuite_credentials = NetSuiteCredentials.objects.filter(workspace_id=workspace.id).first()
+    if netsuite_credentials:
+        for offset in range(0, expense_group_counts, page_size):
+            expense_to_be_updated = []
+            limit = offset + page_size
+            paginated_expense_groups = ExpenseGroup.objects.filter(workspace_id=workspace.id, response_logs__isnull=False)[offset:limit]
+            for expense_group in paginated_expense_groups:
+                expense_group.export_url = generate_netsuite_export_url(response_logs=expense_group.response_logs, netsuite_credentials=netsuite_credentials)
+                expense_group.save()
