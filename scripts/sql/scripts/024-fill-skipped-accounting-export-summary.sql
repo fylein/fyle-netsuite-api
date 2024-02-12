1a9f30937c1d23345491da9a344231bf84d27565
ruuushhh <rushikeshtayade2001@gmail.com>
2024-02-12 21:58:09 +0530

    Updated url

--- scripts/sql/scripts/024-fill-skipped-accounting-export-summary.sql
+++ scripts/sql/scripts/024-fill-skipped-accounting-export-summary.sql
@@ -3,7 +3,7 @@ begin;
 
 update expenses set accounting_export_summary = jsonb_build_object(
     'id', expense_id,
-    'url', 'https://staging1.fyle.tech/app/settings/#/integrations/native_apps?integrationIframeTarget=integrations/intacct/workspaces/main/export_log',
+    'url', CONCAT('https://netsuite.fyleapps.tech/workspaces/', workspace_id, '/expense_groups?page_number=0&page_size=10&state=COMPLETE'),
     'state', 'SKIPPED',
     'synced', false,
     'error_type', null
