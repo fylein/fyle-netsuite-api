f2ff9443891bf42ee826e15cbd661591de02acd9
ruuushhh <rushikeshtayade2001@gmail.com>
2024-03-19 15:45:32 +0530

    fix-tests

--- scripts/python/fill-accounting-export-summary.py
+++ scripts/python/fill-accounting-export-summary.py
@@ -54,7 +54,7 @@ for workspace in workspaces:
                         error_type = 'MAPPING'
                     else:
                         error_type = 'ACCOUNTING_INTEGRATION_ERROR'
-                url = '{}/workspaces/main/dashboard'.format(settings.NETSUITE_INTEGRATION_APP_URL)
+                url = '{}/workspaces/{}/dashboard'.format(settings.NETSUITE_INTEGRATION_APP_URL, expense.workspace_id),
             else:
                 if expense_group.export_url:
                     url = expense_group.export_url
