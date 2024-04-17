47397354f752eeec6b51e2d7d122b7cc584fffe1
anishfyle <anishkumar.s@fyle.in>
2024-04-17 17:29:58 +0530

    added tests

--- tests/test_netsuite/test_models.py
+++ tests/test_netsuite/test_models.py
@@ -57,6 +57,13 @@ def test_get_department_id_or_none(access_token, mocker):
     department_id = get_department_id_or_none(expense_group, None)
     assert department_id == None
 
+def test_get_tax_item_id_or_none(db):
+    expense_group = ExpenseGroup.objects.get(id=1)
+    expenses = expense_group.expenses.all()
+
+    for lineitem in expenses:
+        location_id = get_tax_item_id_or_none(expense_group, lineitem)
+        assert location_id == None
 
 def test_get_ccc_account_id(db, mocker):
     configuration = Configuration.objects.get(workspace_id=49)
