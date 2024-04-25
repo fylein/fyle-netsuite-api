a54651d67365833261c385070950f882a631d730
ruuushhh <rushikeshtayade2001@gmail.com>
2024-04-25 14:20:06 +0530

    fix tests:

--- apps/netsuite/models.py
+++ apps/netsuite/models.py
@@ -98,20 +98,20 @@ def get_class_id_or_none(expense_group: ExpenseGroup, lineitem: Expense):
     return class_id
 
 
-def get_tax_group_mapping(lineitem: Expense = None):
+def get_tax_group_mapping(lineitem: Expense = None, workspace_id: int = None):
     mapping: Mapping = Mapping.objects.filter(
         source_type='TAX_GROUP',
         destination_type='TAX_ITEM',
         source__source_id=lineitem.tax_group_id,
-        workspace_id=lineitem.workspace.id
+        workspace_id=workspace_id
     ).first()
 
     return mapping
 
 
-def get_tax_item_id_or_none(general_mapping: GeneralMapping, lineitem: Expense = None):
+def get_tax_item_id_or_none(expense_group: ExpenseGroup, general_mapping: GeneralMapping, lineitem: Expense = None):
     tax_code = None
-    mapping = get_tax_group_mapping(lineitem)
+    mapping = get_tax_group_mapping(lineitem, expense_group.workspace_id)
 
     if mapping:
         tax_code = mapping.destination.destination_id
@@ -123,7 +123,7 @@ def get_tax_item_id_or_none(general_mapping: GeneralMapping, lineitem: Expense =
 
 def get_tax_info(lineitem: Expense = None):
     tax_code, tax_rate, tax_type = None, None, None
-    mapping = get_tax_group_mapping(lineitem)
+    mapping = get_tax_group_mapping(lineitem, lineitem.workspace_id)
 
     if mapping:
         tax_code = mapping.destination.destination_id
@@ -538,7 +538,7 @@ class BillLineitem(models.Model):
                     'department_id': department_id,
                     'customer_id': customer_id,
                     'amount': lineitem.amount,
-                    'tax_item_id': get_tax_item_id_or_none(general_mappings, lineitem),
+                    'tax_item_id': get_tax_item_id_or_none(expense_group, general_mappings,lineitem),
                     'tax_amount': lineitem.tax_amount,
                     'billable': billable,
                     'memo': get_expense_purpose(lineitem, category, configuration),
@@ -750,7 +750,7 @@ class CreditCardChargeLineItem(models.Model):
                 'department_id': department_id,
                 'customer_id': customer_id,
                 'amount': lineitem.amount,
-                'tax_item_id': get_tax_item_id_or_none(general_mappings, lineitem),
+                'tax_item_id': get_tax_item_id_or_none(expense_group, general_mappings,lineitem),
                 'tax_amount': lineitem.tax_amount,
                 'billable': billable,
                 'memo': get_expense_purpose(lineitem, category, configuration),
@@ -982,7 +982,7 @@ class ExpenseReportLineItem(models.Model):
                     'location_id': location_id,
                     'department_id': department_id,
                     'currency': currency.destination_id if currency else '1',
-                    'tax_item_id': get_tax_item_id_or_none(general_mappings, lineitem),
+                    'tax_item_id': get_tax_item_id_or_none(expense_group, general_mappings,lineitem),
                     'tax_amount': lineitem.tax_amount,
                     'transaction_date': get_transaction_date(expense_group),
                     'memo': get_expense_purpose(lineitem, category, configuration),
@@ -1203,7 +1203,7 @@ class JournalEntryLineItem(models.Model):
                     'class_id': class_id if class_id else None,
                     'entity_id': entity_id,
                     'amount': lineitem.amount,
-                    'tax_item_id': get_tax_item_id_or_none(general_mappings, lineitem),
+                    'tax_item_id': get_tax_item_id_or_none(expense_group, general_mappings,lineitem),
                     'tax_amount': lineitem.tax_amount,
                     'memo': get_expense_purpose(lineitem, category, configuration),
                     'netsuite_custom_segments': custom_segments
