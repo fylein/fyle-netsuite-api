8efe0e982519a7e4217dac99ee2916ab62e78393
Ashutosh619-sudo <ashutoshsinghrkt@gmail.com>
2024-04-29 12:56:47 +0530

    comment resolved and some fix

--- scripts/python/parse-error.py
+++ scripts/python/parse-error.py
@@ -1,23 +1,36 @@
-from apps.netsuite.errors import error_matcher, get_entity_values, replace_destination_id_with_values
-from apps.tasks.models import Error
+from apps.netsuite.errors import (
+    error_matcher,
+    get_entity_values,
+    replace_destination_id_with_values,
+)
+from apps.tasks.models import Error, TaskLog
 from apps.workspaces.models import Configuration
 
-errors_count = Error.objects.filter(type='NETSUITE_ERROR', is_parsed=False).count()
+errors_count = Error.objects.filter(type="NETSUITE_ERROR", is_parsed=False).count()
 print(errors_count)
 page_size = 200
 count = 0
 for offset in range(0, errors_count, page_size):
     limit = offset + page_size
-    paginated_errors = Error.objects.filter(type='NETSUITE_ERROR', is_parsed=False).order_by('id')[offset:limit]
+    paginated_errors = Error.objects.filter(
+        type="NETSUITE_ERROR", is_parsed=False
+    ).order_by("id")[offset:limit]
     for error in paginated_errors:
-        configuration = Configuration.object.filter(workspace_id=error.workspace_id)
+        configuration = Configuration.objects.filter(workspace_id=error.workspace_id).first()
         message = error.error_detail
         fund_source = error.expense_group.fund_source
-        if fund_source == 'PERSONAL':
+        task_log = TaskLog.objects.filter(expense_group_id=error.expense_group.id).first()
+        if fund_source == "PERSONAL":
             configuration_export_type = configuration.reimbursable_expenses_object
         else:
-            configuration_export_type = configuration.corporate_credit_card_expenses_object
-        error_dict, article_link = error_matcher(message=message, export_type=configuration_export_type, configuration=configuration)
+            configuration_export_type = (
+                configuration.corporate_credit_card_expenses_object
+            )
+            error_dict, article_link = error_matcher(
+                message=message,
+                export_type=configuration_export_type,
+                configuration=configuration,
+            )
         entities = get_entity_values(error_dict, error.workspace_id)
         message = replace_destination_id_with_values(message, entities)
 
@@ -25,6 +38,8 @@ for offset in range(0, errors_count, page_size):
             error.is_parsed = True
             error.error_detail = message
             error.article_link = article_link
-            error.expense_group.task_log['message'] = message
+            task_log.detail['message'] = message
             error.save()
             count += 1
+
+print(count)
