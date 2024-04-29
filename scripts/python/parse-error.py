948133d1f1664614d5e19c9d5ed6f3f795268365
Ashutosh619-sudo <ashutoshsinghrkt@gmail.com>
2024-04-29 18:19:00 +0530

    changes to parser script

--- scripts/python/parse-error.py
+++ scripts/python/parse-error.py
@@ -16,30 +16,45 @@ for offset in range(0, errors_count, page_size):
         type="NETSUITE_ERROR", is_parsed=False
     ).order_by("id")[offset:limit]
     for error in paginated_errors:
-        configuration = Configuration.objects.filter(workspace_id=error.workspace_id).first()
+        configuration = Configuration.objects.filter(
+            workspace_id=error.workspace_id
+        ).first()
         message = error.error_detail
         fund_source = error.expense_group.fund_source
-        task_log = TaskLog.objects.filter(expense_group_id=error.expense_group.id).first()
+        task_log = TaskLog.objects.filter(
+            expense_group_id=error.expense_group.id
+        ).first()
+        export_types = {
+            "EXPENSE REPORT": "expense_report",
+            "JOURNAL ENTRY": "journal_entry",
+            "BILL": "bills",
+            "CREDIT CARD CHARGE": "credit_card_charge",
+        }
+
         if fund_source == "PERSONAL":
             configuration_export_type = configuration.reimbursable_expenses_object
         else:
             configuration_export_type = (
                 configuration.corporate_credit_card_expenses_object
             )
-            error_dict, article_link = error_matcher(
-                message=message,
-                export_type=configuration_export_type,
-                configuration=configuration,
-            )
+
+        export_type = export_types[configuration_export_type]
+
+        error_dict, article_link = error_matcher(
+            message=message,
+            export_type=export_type,
+            configuration=configuration,
+        )
         entities = get_entity_values(error_dict, error.workspace_id)
         message = replace_destination_id_with_values(message, entities)
-
         if message:
             error.is_parsed = True
             error.error_detail = message
             error.article_link = article_link
-            task_log.detail['message'] = message
             error.save()
+            if isinstance(task_log.detail, list):
+                task_log.detail[0]["message"] = message
+                task_log.save()
             count += 1
 
 print(count)
