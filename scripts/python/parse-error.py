334352685e06a62d8d20fe278460c82a1b83a86a
Ashutosh619-sudo <ashutoshsinghrkt@gmail.com>
2024-04-29 09:53:15 +0530

    Parse existing error

---
+++ scripts/python/parse-error.py
@@ -0,0 +1,31 @@
+from apps.netsuite.errors import error_matcher, get_entity_values, replace_destination_id_with_values
+from apps.tasks.models import Error
+from apps.workspaces.models import Configuration
+
+errors_count = Error.objects.filter(type='NETSUITE_ERROR', is_resolved=False).count()
+print(errors_count)
+page_size = 200
+count = 0
+for offset in range(0, errors_count, page_size):
+    limit = offset + page_size
+    paginated_errors = Error.objects.filter(type='NETSUITE_ERROR', is_resolved=False).order_by('id')[offset:limit]
+    for error in paginated_errors:
+        configuration = Configuration.object.filter(workspace_id=error.workspace_id)
+        message = error.error_detail
+        fund_source = error.expense_group.fund_source
+        configuration = Configuration.objects.get(workspace_id=error.workspace_id)
+        if fund_source == 'PERSONAL':
+            configuration_export_type = Configuration.objects.get(workspace_id=error.workspace_id).reimbursable_expenses_object
+        else:
+            configuration_export_type = Configuration.objects.get(workspace_id=error.workspace_id).corporate_credit_card_expenses_object
+        error_dict, article_link = error_matcher(message=message, export_type=configuration_export_type, configuration=configuration)
+        entities = get_entity_values(error_dict, error.workspace_id)
+        message = replace_destination_id_with_values(message, entities)
+
+        if message:
+            error.is_parsed = True
+            error.error_detail = message
+            error.article_link = article_link
+            error.expense_group.task_log['message'] = message
+            error.save()
+            count += 1
