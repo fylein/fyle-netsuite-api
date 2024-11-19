ac4695f6ecf44d4b2b833830d269066601d1a200
anishfyle <anishkumar.s@fyle.in>
2024-11-19 17:25:09 +0530

    feat: add masked corporate card number and employee name

--- apps/netsuite/models.py
+++ apps/netsuite/models.py
@@ -267,6 +267,8 @@ def get_expense_purpose(lineitem, category, configuration) -> str:
 
     details = {
         'employee_email': lineitem.employee_email,
+        'employee_name': lineitem.employee_name,
+        'card_number': '{0}'.format(lineitem.masked_corporate_card_number) if lineitem.masked_corporate_card_number else '',
         'merchant': '{0}'.format(lineitem.vendor) if lineitem.vendor else '',
         'category': '{0}'.format(category) if lineitem.category else '',
         'purpose': '{0}'.format(lineitem.purpose) if lineitem.purpose else '',
