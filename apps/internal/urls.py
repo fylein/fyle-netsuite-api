c4a81ea6d289298c74b968274c2546adaf45b733
ashwin1111 <winash1998@gmail.com>
2024-11-22 22:27:40 +0530

    feat: Add internal API for get exported entry

--- apps/internal/urls.py
+++ apps/internal/urls.py
@@ -2,8 +2,9 @@ import itertools
 
 from django.urls import path
 
-from .views import AccountingFieldsView
+from .views import AccountingFieldsView, ExportedEntryView
 
 urlpatterns = [
     path('accounting_fields/', AccountingFieldsView.as_view(), name='accounting-fields'),
+    path('exported_entry/', ExportedEntryView.as_view(), name='exported-entry'),
 ]
