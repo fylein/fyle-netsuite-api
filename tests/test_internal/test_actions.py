bb8c8795aec00efc1aff75606d4f03e4233c64cf
ashwin1111 <winash1998@gmail.com>
2024-11-22 22:14:08 +0530

    tests: get accounting field internal API

---
+++ tests/test_internal/test_actions.py
@@ -0,0 +1,27 @@
+from apps.internal.actions import get_accounting_fields
+from tests.test_netsuite.fixtures import data
+
+def test_get_accounting_fields(db, mocker):
+    query_params = {
+        'org_id': 'or79Cob97KSh',
+        'resource_type': 'employees',
+    }
+    mocker.patch(
+        'netsuitesdk.api.employees.Employees.get_all_generator',
+        return_value=data['get_all_employees']    
+    )
+
+    mocker.patch('netsuitesdk.api.currencies.Currencies.get_all')
+
+    mocker.patch(
+        'netsuitesdk.api.custom_lists.CustomLists.get',
+        return_value=data['get_custom_list']
+    )
+
+    fields = get_accounting_fields(query_params)
+    assert fields is not None
+
+    query_params['resource_type'] = 'custom_lists'
+    query_params['internal_id'] = '1'
+    fields = get_accounting_fields(query_params)
+    assert fields is not None
