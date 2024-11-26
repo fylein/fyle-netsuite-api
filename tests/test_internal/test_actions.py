757cfc571bc982666719836e764cbb3478866271
Ashwin Thanaraj <37061471+ashwin1111@users.noreply.github.com>
2024-11-26 11:41:46 +0530

    feat: Add internal API for get exported entry (#679)
    * feat: Add internal API for get exported entry
    
    * add tests

--- tests/test_internal/test_actions.py
+++ tests/test_internal/test_actions.py
@@ -1,4 +1,4 @@
-from apps.internal.actions import get_accounting_fields
+from apps.internal.actions import get_accounting_fields, get_exported_entry
 from tests.test_netsuite.fixtures import data
 
 def test_get_accounting_fields(db, mocker):
@@ -25,3 +25,18 @@ def test_get_accounting_fields(db, mocker):
     query_params['internal_id'] = '1'
     fields = get_accounting_fields(query_params)
     assert fields is not None
+
+
+def test_get_exported_entry(db, mocker):
+    query_params = {
+        'org_id': 'or79Cob97KSh',
+        'resource_type': 'vendor_bills',
+        'internal_id': '1'
+    }
+    mocker.patch(
+        'netsuitesdk.api.vendor_bills.VendorBills.get',
+        return_value={'summa': 'hehe'}
+    )
+
+    entry = get_exported_entry(query_params)
+    assert entry is not None
