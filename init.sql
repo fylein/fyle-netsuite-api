37b6f25f388c3bd6d6d42062a40c3b72765ac87a
Rushi <rushi@Rushis-MacBook-Air.local>
2025-03-11 14:21:39 +0530

    fix: TLS vulnerability

--- init.sql
+++ init.sql
@@ -1,1 +1,11 @@
+-- Create the main application database
 CREATE DATABASE netsuite_db;
+
+-- Create the test database
+CREATE DATABASE test_netsuite_db;
+
+-- Connect to test database and load test data
+\c test_netsuite_db;
+
+-- Load the test data
+\i /docker-entrypoint-initdb.d/reset_db.sql
--- tests/conftest.py
+++ tests/conftest.py
@@ -12,8 +12,6 @@ from fyle_netsuite_api.tests import settings
 
 from .test_fyle.fixtures import data as fyle_data
 
-def pytest_configure():
-    os.system('sh ./tests/sql_fixtures/reset_db_fixtures/reset_db.sh')
 
 @pytest.fixture
 def api_client():
