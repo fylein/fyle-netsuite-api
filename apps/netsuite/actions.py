94c242317d18c0bf2aac54c2e67a73aa7b7eda22
Rushi <rushi@Rushis-MacBook-Air.local>
2025-03-13 12:23:01 +0530

    fix: Add 1 general post accounting call after last export

--- apps/netsuite/actions.py
+++ apps/netsuite/actions.py
@@ -1,12 +1,13 @@
 from apps.tasks.models import TaskLog
-from apps.workspaces.models import LastExportDetail
+from apps.workspaces.models import LastExportDetail, Workspace
 from django.db.models import Q
 
 from apps.workspaces.tasks import patch_integration_settings
-
+from apps.fyle.tasks import post_accounting_export_summary
 
 def update_last_export_details(workspace_id):
     last_export_detail = LastExportDetail.objects.get(workspace_id=workspace_id)
+    workspace = Workspace.objects.get(id=workspace_id)
 
     failed_exports = TaskLog.objects.filter(
         ~Q(type__in=['CREATING_VENDOR_PAYMENT','FETCHING_EXPENSES']), workspace_id=workspace_id, status__in=['FAILED', 'FATAL']
@@ -24,5 +25,6 @@ def update_last_export_details(workspace_id):
     last_export_detail.total_expense_groups_count = failed_exports + successful_exports
     last_export_detail.save()
     patch_integration_settings(workspace_id, errors=failed_exports)
+    post_accounting_export_summary(workspace.fyle_org_id, workspace_id)
 
     return last_export_detail
