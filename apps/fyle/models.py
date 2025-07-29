3b3e90bf775b994f800f6a28b80d4e409138fcc6
ruuushhh <66899387+ruuushhh@users.noreply.github.com>
2025-07-29 14:48:29 +0530

    fix: tax amount None (#830)
    (cherry picked from commit e8c472651f1d83da77be93e0a50558133da199eb)

--- apps/fyle/models.py
+++ apps/fyle/models.py
@@ -149,7 +149,6 @@ class Expense(models.Model):
         expense_objects = []
 
         for expense in expenses:
-            print("expense", expense)
             for custom_property_field in expense['custom_properties']:
                 if expense['custom_properties'][custom_property_field] == '':
                     expense['custom_properties'][custom_property_field] = None
@@ -163,6 +162,9 @@ class Expense(models.Model):
                     'expense_created_at': expense['expense_created_at'],
                     'expense_updated_at': expense['expense_updated_at']
                 }
+
+            if expense['tax_group_id'] is not None and expense['tax_amount'] is None:
+                expense['tax_amount'] = 0
 
             defaults = {
                 'employee_email': expense['employee_email'],
