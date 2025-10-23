772a203886b284ee1522b7d85c394a6477adc5c1
Ashutosh619-sudo <ashutosh.s@fyle.com>
2025-10-23 15:36:51 +0530

    comment resolved

--- apps/netsuite/models.py
+++ apps/netsuite/models.py
@@ -496,9 +496,8 @@ class BillLineitem(models.Model):
                 if employee_mapping and employee_mapping.destination_employee:
                     class_id = employee_mapping.destination_employee.detail.get('class_id')
 
-            if not class_id:
-                if general_mappings.class_id and general_mappings.class_level in ['TRANSACTION_LINE', 'ALL']:
-                    class_id = general_mappings.class_id
+            if not class_id and general_mappings.class_id and general_mappings.class_level in ['TRANSACTION_LINE', 'ALL']:
+                class_id = general_mappings.class_id
 
             department_id = get_department_id_or_none(expense_group, lineitem)
 
@@ -715,9 +714,8 @@ class CreditCardChargeLineItem(models.Model):
                 if employee_mapping and employee_mapping.destination_employee:
                     class_id = employee_mapping.destination_employee.detail.get('class_id')
 
-            if not class_id:
-                if general_mappings.class_id and general_mappings.class_level in ['TRANSACTION_LINE', 'ALL']:
-                    class_id = general_mappings.class_id
+            if not class_id and general_mappings.class_id and general_mappings.class_level in ['TRANSACTION_LINE', 'ALL']:
+                class_id = general_mappings.class_id
 
             department_id = get_department_id_or_none(expense_group, lineitem)
 
@@ -957,9 +955,8 @@ class ExpenseReportLineItem(models.Model):
             if not class_id and general_mappings.use_employee_class and employee_field_mapping == 'EMPLOYEE':
                 class_id = entity.destination_employee.detail.get('class_id')
 
-            if not class_id:
-                if general_mappings.class_id and general_mappings.class_level in ['TRANSACTION_LINE', 'ALL']:
-                    class_id = general_mappings.class_id
+            if not class_id and general_mappings.class_id and general_mappings.class_level in ['TRANSACTION_LINE', 'ALL']:
+                class_id = general_mappings.class_id
 
             if not department_id and general_mappings.use_employee_department and \
                 general_mappings.department_level in ('ALL', 'TRANSACTION_LINE') and \
@@ -1184,9 +1181,8 @@ class JournalEntryLineItem(models.Model):
             if not class_id and general_mappings.use_employee_class and employee_field_mapping == 'EMPLOYEE' and employee_mapping and employee_mapping.destination_employee:
                 class_id = employee_mapping.destination_employee.detail.get('class_id')
 
-            if not class_id:
-                if general_mappings.class_id and general_mappings.class_level in ['TRANSACTION_LINE', 'ALL']:
-                    class_id = general_mappings.class_id
+            if not class_id and general_mappings.class_id and general_mappings.class_level in ['TRANSACTION_LINE', 'ALL']:
+                class_id = general_mappings.class_id
             
             if not department_id and general_mappings.use_employee_department and general_mappings.department_level in ('ALL', 'TRANSACTION_LINE') \
                 and employee_field_mapping == 'EMPLOYEE'and employee_mapping and employee_mapping.destination_employee:  
