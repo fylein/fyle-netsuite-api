1cb1fee2d0d05518903ccbc9b25b352ddabb3b68
ashwin1111 <winash1998@gmail.com>
2025-11-18 14:04:11 +0530

    chore: remove unused orgs API

--- apps/users/views.py
+++ apps/users/views.py
@@ -36,23 +36,3 @@ class UserProfileView(generics.RetrieveAPIView):
             data=employee_profile,
             status=status.HTTP_200_OK
         )
-
-
-class FyleOrgsView(generics.ListCreateAPIView):
-    """
-    FyleOrgs view
-    """
-
-    permission_classes = [IsAuthenticated]
-
-    def get(self, request, *args, **kwargs):
-        """
-        Get cluster domain from Fyle
-        """
-        cluster_domain, refresh_token = get_cluster_domain_and_refresh_token(request.user)
-        fyle_orgs = get_fyle_orgs(refresh_token, cluster_domain)
-
-        return Response(
-            data=fyle_orgs,
-            status=status.HTTP_200_OK
-        )
