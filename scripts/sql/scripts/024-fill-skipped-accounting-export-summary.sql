e5e0c7c932e39c1bcc9d6304e1522d7d98fe5d79
Shubham <shubham.b@fyle.in>
2026-02-17 21:46:19 +0530

    chore: Clean hash urls
    Clickup
    https://app.clickup.com/

--- scripts/sql/scripts/024-fill-skipped-accounting-export-summary.sql
+++ scripts/sql/scripts/024-fill-skipped-accounting-export-summary.sql
@@ -3,7 +3,7 @@ begin;
 
 update expenses set accounting_export_summary = jsonb_build_object(
     'id', expense_id,
-    'url', CONCAT('https://app.fylehq.com/app/settings/#/integrations/native_apps?integrationIframeTarget=integrations/netsuite/main/export_log'),
+    'url', CONCAT('https://app.fylehq.com/app/settings/integrations/native_apps?integrationIframeTarget=integrations/netsuite/main/export_log'),
     'state', 'SKIPPED',
     'synced', false,
     'error_type', null
